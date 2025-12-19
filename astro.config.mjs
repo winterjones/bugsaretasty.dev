76334fb9f1d29496de83e140dfba30b37c1f57ad
Winter Jones <winterjones1908@gmail.com>
2025-12-19 21:26:19 +0000

    update config mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     prefetch: true,
-    site: "bugsaretasty.dev"
+    base: "/bugsaretasty.dev",
+    site: "winterjones.github.io"
 });
